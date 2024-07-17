0be9d28d44289c1ac00763ecfb1adf0a3ea30c77
z4hid <zahidhasan9297@gmail.com>
2024-07-17 08:34:16 +0600

    constants added

---
+++ src/ThreatDetector/constants/__init__.py
@@ -0,0 +1,20 @@
+import os
+import torch
+from from_root import from_root
+
+ARTIFACTS_DIR: str = 'artifacts'
+SOURCE_DIR_NAME: str = 'src'
+
+ZIP_FILE_NAME: str = 'data.zip'
+TRAIN_FOLDER_NAME: str = 'train'
+TEST_FOLDER_NAME: str = 'test'
+VALIDATION_FOLDER_NAME: str = 'valid'
+
+# Data Ingestion related constants
+ARTIFACTS_DIR = os.path.join(from_root(), 'artifacts')
+BUCKET_NAME = 'dumpware10'
+CLOUD_DATA_FOLDER_NAME = 'data.zip'
+DATA_INGESTION_ARTIFACTS_DIR = 'DataIngestion'
+UNZIP_FOLDER_NAME = 'data/'
+
+
