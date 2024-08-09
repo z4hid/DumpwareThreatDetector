82c15852e5d11e260eb6b37d1f00695f0fc70433
z4hid <zahidhasan9297@gmail.com>
2024-08-09 17:10:40 +0600

    data ingestion artifacts entity added

---
+++ src/ThreatDetector/entity/artifact_entity.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class DataIngestionArtifact:
+    train_file_path: str
+    test_file_path: str
+    valid_file_path: str
+    data_path: str
+    
+    
+
