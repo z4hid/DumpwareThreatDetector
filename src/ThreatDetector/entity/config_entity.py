2c8875484191e3b101e55dece8d0e54a861fb98a
z4hid <zahidhasan9297@gmail.com>
2024-08-13 20:56:30 +0600

    config updated

--- src/ThreatDetector/entity/config_entity.py
+++ src/ThreatDetector/entity/config_entity.py
@@ -6,15 +6,48 @@ from src.ThreatDetector.constants import *
 from datetime import datetime
 from from_root import from_root
 
+# Generate a timestamp for file naming
 TIMESTAMP: str = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
+
 
 @dataclass
 class DataIngestionConfig:
-    data_ingestion_artifact_dir: str = os.path.join(from_root(), ARTIFACTS_DIR, DATA_INGESTION_ARTIFACTS_DIR)
-    # zip_data_path: str = os.path.join(data_ingestion_artifact_dir, ZIP_FILE_NAME) # CLOUD_DATA_FOLDER_NAME
-    zip_data_path: str = 'https://drive.google.com/file/d/1-xFGicUDjIjeh3EdMbESzOYMywAOyotW/view'
-    train_data_path: str = os.path.join(data_ingestion_artifact_dir, TRAIN_FOLDER_NAME)
-    test_data_path: str = os.path.join(data_ingestion_artifact_dir, TEST_FOLDER_NAME)
-    valid_data_path: str = os.path.join(data_ingestion_artifact_dir, VALIDATION_FOLDER_NAME)
-    
-    
+    """
+    Data class for storing configuration parameters related to data ingestion.
+    """
+
+    # Path where the data ingestion artifacts are stored
+    data_ingestion_artifact_dir: str = os.path.join(
+        from_root(), ARTIFACTS_DIR, DATA_INGESTION_ARTIFACTS_DIR
+    )
+
+    # URL to the zip file containing the training and test data
+    zip_data_path: str = (
+        'https://drive.google.com/file/d/1-xFGicUDjIjeh3EdMbESzOYMywAOyotW/view'
+    )
+
+    # Path to the directory containing the training data
+    train_data_path: str = os.path.join(
+        data_ingestion_artifact_dir, TRAIN_FOLDER_NAME
+    )
+
+    # Path to the directory containing the test data
+    test_data_path: str = os.path.join(
+        data_ingestion_artifact_dir, TEST_FOLDER_NAME
+    )
+
+    # Path to the directory containing the validation data
+    valid_data_path: str = os.path.join(
+        data_ingestion_artifact_dir, VALIDATION_FOLDER_NAME
+    )
+
+
+@dataclass
+class DataValidationConfig:
+    """
+    Data class for storing configuration parameters related to data validation.
+    """
+
+    # Path to the schema file
+    schema_file_path: str = os.path.join(from_root(), SCHEMA_FILE_PATH)
+
