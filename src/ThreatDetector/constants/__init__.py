2786476aaa329e5d2c550244f988e2eddbd0f7f0
z4hid <zahidhasan9297@gmail.com>
2024-09-01 08:10:18 +0600

    model training contants updated

--- src/ThreatDetector/constants/__init__.py
+++ src/ThreatDetector/constants/__init__.py
@@ -18,7 +18,17 @@ DATA_INGESTION_ARTIFACTS_DIR = 'DataIngestion'
 UNZIP_FOLDER_NAME = 'data/'
 
 
-# Data validation related config
+# Data validation related constants
 SCHEMA_FILE_PATH = os.path.join(from_root(), 'config', 'schema.yaml')
 
-
+# Model Training related constants
+MODEL_TRAINING_ARTIFACTS_DIR: str = 'ModelTraining'
+MODEL_NAME: str = 'model.pth'
+BATCH_SIZE: int = 32
+EPOCHS = 10
+LEARNING_RATE = 0.0001
+GRAD_CLIP: float = 0.1
+WEIGHT_DECAY: float = 1e-4
+IN_CHANNELS: int = 3
+OPTIMIZER = torch.optim.Adam
+NUM_CLASSES: int = 86
