3be9611d1ccc57d9b06acbb40261ae850627a759
z4hid <zahidhasan9297@gmail.com>
2024-09-06 04:31:30 +0600

    artifact entity updated

--- src/ThreatDetector/entity/artifact_entity.py
+++ src/ThreatDetector/entity/artifact_entity.py
@@ -13,3 +13,24 @@ class DataIngestionArtifact:
 class DataValidationArtifact:
     validation_status: bool
     
+@dataclass
+class ModelTrainingArtifacts:
+    model_path: str
+    result: dict
+    transformer_object_path: str
+     
+@dataclass
+class ModelEvaluationArtifacts:
+    hf_model_loss: float
+    is_model_accepted: bool
+    trained_model_path: str
+    hf_model_path: str
+    
+
+@dataclass
+class ModelPusherArtifcats:
+    response: dict
+    
+@dataclass
+class PredictionArtifact:
+    predictions: list
